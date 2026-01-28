7617578105113ef7bb9a90d6659eca52ec2585d8
pnkhanh <pnkhanh@pnkhanhs-MacBook-Pro.local>
2026-01-28 14:03:14 +0700

    Add manifest file for BE and FE

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,10 @@
 export default {
-    HOST: process.env.DB_HOST || "localhost",
-    USER: process.env.DB_USER || "root",
-    PASSWORD: process.env.DB_PASSWORD || "",
-    DB: process.env.DB_NAME || "testdb",
-    dialect: process.env.DB_DIALECT || "mysql",
+    HOST: process.env.DB_HOST, //|| "localhost",
+    USER: process.env.DB_USER, //|| "root",
+    PASSWORD: process.env.DB_PASSWORD, //|| "",
+    DB: process.env.DB_NAME, //|| "testdb",
+    PORT: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT, //|| "mysql",
     pool: {
         max: 5,
         min: 0,
